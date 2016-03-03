4868d22cbb682e4ac83fd7a01c09b06b5ef257df
adriana <nana171802@gmail.com>
2016-03-02 20:52:11 -0500

    noveno cambio

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -109,6 +109,29 @@ end
 
 
 class Backstage < Base
+
+  private
+
+  def update_quality
+    quality = if item.sell_in > 10
+                item.quality + 1
+              elsif item.sell_in > 5
+                item.quality + 2
+              elsif item.sell_in > 0
+                item.quality + 3
+              else
+                0
+              end
+
+    item.quality = quality if quality <= 50
+    item.quality = 50 if quality > 50
+
+  end
+
+  def update_sell_in
+    item.sell_in -= 1
+  end
+
 end
 
 class Sulfuras < Base
