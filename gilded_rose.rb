1a80d32c54baa9115d1905a91b2f5eaec175ded0
adriana <nana171802@gmail.com>
2016-03-02 20:38:33 -0500

    octavo cambio

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -95,7 +95,18 @@ class Default < Base
 end
 
 class Conjured < Base
+  private
+
+  def update_quality
+    quality = item.quality - 2
+    item.quality = quality if quality >= 0
+  end
+
+  def update_sell_in
+    item.sell_in -= 1
+  end
 end
+
 
 class Backstage < Base
 end
